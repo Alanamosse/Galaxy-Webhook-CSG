f9b92f878555f993833a63fcb773d02f952494c9
Alanamosse <alan.amosse@gmail.com>
2018-06-29 14:07:53 -0700

    test2

---
+++ static/script2.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+var pages=new Array();
+ pages[0]="https://citizensort.org/livinglinks/hmc.php";
+ pages[1]="https://citizensort.org/web.php/forgottenisland";
+ pages[2]="https://github.com/ValentinChCloud/Galaxy-Webhook-CSG/pull/2";
+ pages[3]="http://lelombrik.net/77522";
+
+var rand=Math.floor(Math.random()*pages.length);
+
+var PHDComicsAppView = Backbone.View.extend({
+        el : '#itch',
+
+                    appTemplate: _.template(
+                '<iframe id="myFrame" src=page[rand] style="height:600px;width:100%"></iframe>'
+            ),
+            initialize: function() {
+            this.$el.html(this.appTemplate());
+        },
+
+});
+
+new PHDComicsAppView();
+
+});
