2567f81447202621b280609fc509715737e974b5
Alanamosse <alan.amosse@gmail.com>
2018-06-29 13:23:29 -0700

    first random func to select url

--- static/script.js
+++ static/script.js
@@ -1,15 +1,35 @@
-
 $(document).ready(function() {
-var PHDComicsAppView = Backbone.View.extend({
-	el : '#itch',
 
-	            appTemplate: _.template(
-		'<iframe id="myFrame" src="https://citizensort.org/livinglinks/hmc.php" style="height:600px;width:100%"></iframe>'
+var PHDComicsAppView = Backbone.View.extend({
+        el : '#itch',
+
+                    appTemplate: _.template(
+                '<iframe id="myFrame" src="https://citizensort.org/livinglinks/hmc.php" style="height:600px;width:100%"></iframe>'
             ),
             initialize: function() {
-            this.$el.html(this.appTemplate());	   
-	},
+            this.$el.html(this.appTemplate());
+        },
 
-});
-new PHDComicsAppView();
+    });
+var AppView2 = Backbone.View.extend({
+        el : '#itch',
+
+                    appTemplate: _.template(
+                '<iframe id="myFrame" src="https://citizensort.org/web.php/forgottenisland" style="height:600px;width:100%"></iframe>'
+            ),
+            initialize: function() {
+            this.$el.html(this.appTemplate());
+        },
+
+    });
+
+var rand=Math.random();
+console.log(rand);
+
+if (rand>=0.5) {
+    new PHDComicsAppView();
+}else{
+    new AppView2();
+}
+
 });
